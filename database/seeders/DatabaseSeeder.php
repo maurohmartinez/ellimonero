93723f51f97cdfeda32c8ff27aae11970ddc2893
Mauro Martinez <maurohmartinez@gmail.com>
2021-01-06 14:05:22 +0200

    More

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -26,5 +26,76 @@ class DatabaseSeeder extends Seeder
         if(env('APP_ENV') === 'local'){
             User::factory(10)->create();
         }
+
+        DB::table('menu_items')->insert([
+            'name' => 'Home',
+            'type' => 'internal_link',
+            'link' => '/',
+            'lft' => 2,
+            'rgt' => 3,
+            'depth' => 1,
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s')
+        ]);
+        DB::table('menu_items')->insert([
+            'name' => 'Nosotros',
+            'type' => 'internal_link',
+            'link' => '#nosotros',
+            'lft' => 4,
+            'rgt' => 5,
+            'depth' => 1,
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s')
+        ]);
+        DB::table('menu_items')->insert([
+            'name' => 'Tienda',
+            'type' => 'internal_link',
+            'link' => '#tienda',
+            'lft' => 6,
+            'rgt' => 7,
+            'depth' => 1,
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s')
+        ]);
+        DB::table('menu_items')->insert([
+            'name' => 'Blog',
+            'type' => 'internal_link',
+            'link' => '#blog',
+            'lft' => 8,
+            'rgt' => 9,
+            'depth' => 1,
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s')
+        ]);
+        DB::table('menu_items')->insert([
+            'name' => 'Con opciones',
+            'type' => 'internal_link',
+            'link' => '#opciones',
+            'lft' => 10,
+            'rgt' => 15,
+            'depth' => 1,
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s')
+        ]);
+        DB::table('menu_items')->insert([
+            'name' => 'Opción 1',
+            'type' => 'internal_link',
+            'link' => '#',
+            'lft' => 11,
+            'rgt' => 12,
+            'depth' => 2,
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s')
+        ]);
+        DB::table('menu_items')->insert([
+            'name' => 'Opción 2',
+            'type' => 'internal_link',
+            'link' => '#',
+            'lft' => 13,
+            'rgt' => 14,
+            'depth' => 2,
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s')
+        ]);
     }
 }
