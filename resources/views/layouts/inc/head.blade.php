9842642e33661df65956bdb3b2be9f1e5f4e1431
Mauro Martinez <maurohmartinez@gmail.com>
2021-01-09 22:50:15 +0200

    More

--- resources/views/layouts/inc/head.blade.php
+++ resources/views/layouts/inc/head.blade.php
@@ -37,4 +37,4 @@
 <!-- <link href="https://via.placeholder.com/1000x600.png?text=IMAGE" rel="apple-touch-icon"> -->
 <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
 @yield('styles')
-<livewire:styles />
+@livewireStyles
