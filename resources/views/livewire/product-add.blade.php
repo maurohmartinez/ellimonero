3e20a14be780e85d75e7c2b0726a84ef9c184558
Mauro Martinez <maurohmartinez@gmail.com>
2021-01-13 12:01:01 +0200

    More

--- resources/views/livewire/product-add.blade.php
+++ resources/views/livewire/product-add.blade.php
@@ -1,22 +1,23 @@
 <div>
     <div class="d-flex align-items-center justify-content-between">
-        <div style="background-color: #161616;">
+        <!-- <div style="background-color: #161616;">
             <button wire:click="minus" class="btn btn-dark mt-0 px-0" style="display: inline; width: 50px;" key="button-minus">
                 <i wire:loading.remove wire:target="minus" class="la la-minus"></i>
                 <div wire:loading wire:target="minus" class="spinner-border text-primary" style="zoom: .5;" role="status"></div>
             </button>
-            <p class="text-light px-3" style="border: none; border-radius: 0px; display: inline;">{{ $quantity }}</p>
+            <p class="text-light px-3" style="border: none; border-radius: 0px; display: inline;">{{-- $quantity --}}</p>
             <button wire:click="plus" class="btn btn-dark mt-0 px-0" style="display: inline; width: 50px;" key="button-plus">
                 <i wire:loading.remove wire:target="plus" class="la la-plus"></i>
                 <div wire:loading wire:target="plus" class="spinner-border text-primary" style="zoom: .5;" role="status"></div>
             </button>
-        </div>
-        <div class="d-flex justify-content-end">
-            <span class="ml-3"></span>
-            <div wire:click="addProduct" class="cursor-pointer">
-                <span wire:loading wire:target="addProduct" class="spinner-border text-primary mr-1" style="zoom: .7;" role="status"></span>
+        </div> -->
+        <div wire:click="addProduct" class="d-flex justify-content-end align-items-center cursor-pointer">
+            <!-- <span class="ml-3"></span> -->
+            <div class="mr-1">
+                <span wire:loading wire:target="addProduct" class="spinner-border text-primary mr-1" style="zoom: .9;" role="status"></span>
                 <span wire:loading.remove wire:target="addProduct"><i class="la la-cart-plus" style="font-size: 30px;"></i></span>
             </div>
+            <span>Agregar</span>
         </div>
     </div>
 </div>
--- resources/views/livewire/product-single-add.blade.php
+++ resources/views/livewire/product-single-add.blade.php
@@ -1,17 +1,17 @@
 <div>
     <div class="d-flex align-items-center justify-content-start">
-        <div style="background-color: #161616;">
+        <!-- <div style="background-color: #161616;">
             <button wire:click="minus" class="btn btn-dark mt-0 px-0" style="display: inline; width: 50px;" key="button-minus">
                 <i wire:loading.remove wire:target="minus" class="la la-minus"></i>
                 <div wire:loading wire:target="minus" class="spinner-border text-primary" style="zoom: .5;" role="status"></div>
             </button>
-            <p class="text-light px-3" style="border: none; border-radius: 0px; display: inline;">{{ $quantity }}</p>
+            <p class="text-light px-3" style="border: none; border-radius: 0px; display: inline;">{{-- $quantity --}}</p>
             <button wire:click="plus" class="btn btn-dark mt-0 px-0" style="display: inline; width: 50px;" key="button-plus">
                 <i wire:loading.remove wire:target="plus" class="la la-plus"></i>
                 <div wire:loading wire:target="plus" class="spinner-border text-primary" style="zoom: .5;" role="status"></div>
             </button>
-        </div>
-        <button type="button" wire:click="addProduct" class="btn btn-md btn-primary d-flex align-items-center ml-3" style="padding-top: 6px; padding-bottom: 6px;">
+        </div> -->
+        <button type="button" wire:click="addProduct" class="btn btn-md btn-primary d-flex align-items-center" style="padding-top: 6px; padding-bottom: 6px;">
             <div wire:loading wire:target="addProduct" class="spinner-border text-dark" style="zoom: .8;" role="status"></div>
             <i wire:loading.remove wire:target="addProduct" class="la la-cart-plus" style="font-size: 25px;"></i>
             <span class="ml-2">AGREGAR</span>
