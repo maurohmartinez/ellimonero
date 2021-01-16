c53330de3678ec0e9487b0e139496b9b2bac9e02
Mauro Martinez <maurohmartinez@gmail.com>
2021-01-16 22:38:48 +0200

    More

--- resources/views/qr/error.blade.php
+++ resources/views/qr/error.blade.php
@@ -9,11 +9,13 @@
                     <img src="{{ asset('images/logos/logo-800-w.png') }}" alt="" style="max-height: 150px; width: auto;">
                 </a>
             </div>
-            <div class="alert alert-danger d-flex align-items-center mb-4">
-                <img src="{{ asset('images/qr.png') }}" alt="" class="mr-3" style="max-height: 50px; width: auto;">
-                <p class="font-weight-light pb-0 text-light">{{ $feedback }}</p>
+            <div class="px-4 mt-4">
+                <div class="alert alert-danger d-flex align-items-center mb-4">
+                    <img src="{{ asset('images/qr.png') }}" alt="" class="mr-3" style="max-height: 50px; width: auto;">
+                    <p class="font-weight-light pb-0 text-light">{{ $feedback }}</p>
+                </div>
+                <h5 class="text-center mt-3">Te invitamos a <a href="{{ route('home') }}">continuar navegar nuestro sitio</a> y conocer todos nuestros productos.</h5>
             </div>
-            <h5 class="text-center mt-3">Te invitamos a <a href="{{ route('home') }}">continuar navegar nuestro sitio</a> y conocer todos nuestros productos.</h5>
         </div>
     </div>
 </div>
