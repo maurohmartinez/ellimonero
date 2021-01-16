0b0ea29e6ce33f7be63cb46f34e7f5a3bf09d69c
Mauro Martinez <maurohmartinez@gmail.com>
2021-01-16 22:28:32 +0200

    More

--- resources/views/home/inc/banner_sub.blade.php
+++ resources/views/home/inc/banner_sub.blade.php
@@ -1,6 +1,6 @@
 <section class="px-0 mb-4">
     <div class="main-btns">
-        <img src="{{ asset('images/subasta_banner.jpg') }}" style="width: 100%; height: auto;">
+        <img src="{{ asset('images/subasta_banner_final.jpg') }}" style="width: 100%; height: auto;">
     </div>
     <div class="p-1"></div>
 </section>
