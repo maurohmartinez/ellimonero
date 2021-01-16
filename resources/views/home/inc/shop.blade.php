356a3b881aeacae3d83074e148285b836e6c4f58
Mauro Martinez <maurohmartinez@gmail.com>
2021-01-16 15:40:42 +0200

    More

--- resources/views/home/inc/shop.blade.php
+++ resources/views/home/inc/shop.blade.php
@@ -23,7 +23,7 @@
                             @if($product->stock == 0)
                             <img style="position: absolute; width: 200px;" src="{{ asset('images/sold-out-banner.png') }}" alt="Sold Out">
                             @endif
-                            @if($product->images[0])
+                            @if(count($product->images) > 0)
                             <img src="{{ asset($product->images[0]['image_url']) }}" alt="">
                             @endif
                         </div>
