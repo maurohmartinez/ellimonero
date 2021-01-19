144e99f60fbb603a9010c3e824efb4693a9aecd5
Mauro Martinez <maurohmartinez@gmail.com>
2021-01-19 19:11:40 +0200

    More

--- resources/views/cita/inc/cards.blade.php
+++ resources/views/cita/inc/cards.blade.php
@@ -2,7 +2,7 @@
     <div class="container">
         <div class="spc-single-card-wrapper mb-4">
             <div class="spc-sc-img">
-                <a href="https://www.youtube.com/watch?v=N3gZk4-0KF0&feature=youtu.be" style="position: relative;">
+                <a href="https://youtu.be/LZEFNUUJjMA" style="position: relative;">
                     <i class="la la-youtube text-light" style="position: absolute; font-size: 50px; top: 50%; left: 50%; transform: translate(-50%, -50%);"></i>
                     <img src="{{ asset('images/cita/leo_garcia.jpg') }}" alt="Leo García">
                 </a>
@@ -15,13 +15,13 @@
                     <h5>Episodio 1</h5>
                     <h3>Leo García</h3>
                     <p>Estamos acá para aprender qué es el amor.</p>
-                    <a class="btn btn-primary mt-3" href="https://www.youtube.com/watch?v=N3gZk4-0KF0&feature=youtu.be"><i class="la la-youtube"></i> Ver episodio</a>
+                    <a class="btn btn-primary mt-3" href="https://youtu.be/LZEFNUUJjMA"><i class="la la-youtube"></i> Ver episodio</a>
                 </div>
             </div>
         </div>
         <div class="spc-single-card-wrapper mb-4">
             <div class="spc-sc-img">
-                <a href="https://www.youtube.com/watch?v=edwzJclKq9U&feature=youtu.be" style="position: relative;">
+                <a href="https://youtu.be/Cuo1Gq84HIU" style="position: relative;">
                     <i class="la la-youtube text-light" style="position: absolute; font-size: 50px; top: 50%; left: 50%; transform: translate(-50%, -50%);"></i>
                     <img src="{{ asset('images/cita/miranda.jpg') }}" alt="Miranda">
                 </a>
@@ -34,13 +34,13 @@
                     <h5>Episodio 2</h5>
                     <h3>Miranda</h3>
                     <p>Cuando hacés las cosas con amor, el amor siempre vuelve.</p>
-                    <a class="btn btn-primary mt-3" href="https://www.youtube.com/watch?v=edwzJclKq9U&feature=youtu.be"><i class="la la-youtube"></i> Ver episodio</a>
+                    <a class="btn btn-primary mt-3" href="https://youtu.be/Cuo1Gq84HIU"><i class="la la-youtube"></i> Ver episodio</a>
                 </div>
             </div>
         </div>
         <div class="spc-single-card-wrapper mb-4">
             <div class="spc-sc-img">
-                <a href="https://www.youtube.com/watch?v=HBw0IOlNJA0&feature=youtu.be" style="position: relative;">
+                <a href="https://youtu.be/vW596BO2cxk" style="position: relative;">
                     <i class="la la-youtube text-light" style="position: absolute; font-size: 50px; top: 50%; left: 50%; transform: translate(-50%, -50%);"></i>
                     <img src="{{ asset('images/cita/javier_calamaro.jpg') }}" alt="">
                 </a>
@@ -53,13 +53,13 @@
                     <h5>Episodio 4</h5>
                     <h3>Javier Calamaro</h3>
                     <p>El amor y la pasión son la misma cosa.</p>
-                    <a class="btn btn-primary mt-3" href="https://www.youtube.com/watch?v=HBw0IOlNJA0&feature=youtu.be"><i class="la la-youtube"></i> Ver episodio</a>
+                    <a class="btn btn-primary mt-3" href="https://youtu.be/vW596BO2cxk"><i class="la la-youtube"></i> Ver episodio</a>
                 </div>
             </div>
         </div>
         <div class="spc-single-card-wrapper">
             <div class="spc-sc-img">
-                <a href="https://www.youtube.com/watch?v=UYi8-Tu16Rc&feature=youtu.be" style="position: relative;">
+                <a href="https://youtu.be/ZDr9M7PngOA" style="position: relative;">
                     <i class="la la-youtube text-light" style="position: absolute; font-size: 50px; top: 50%; left: 50%; transform: translate(-50%, -50%);"></i>
                     <img src="{{ asset('images/cita/fabi_cantilo.jpg') }}" alt="">
                 </a>
@@ -72,7 +72,7 @@
                     <h5>Episodio 6</h5>
                     <h3>Fabi Cantilo</h3>
                     <p>El amor nos va a salvar.</p>
-                    <a class="btn btn-primary mt-3" href="https://www.youtube.com/watch?v=UYi8-Tu16Rc&feature=youtu.be"><i class="la la-youtube"></i> Ver episodio</a>
+                    <a class="btn btn-primary mt-3" href="https://youtu.be/ZDr9M7PngOA"><i class="la la-youtube"></i> Ver episodio</a>
                 </div>
             </div>
         </div>
