695742d57f54b90f85dfba9b55fc231325ab6b72
Mauro Martinez <maurohmartinez@gmail.com>
2021-01-27 09:38:19 +0200

    More

--- resources/views/home/inc/links.blade.php
+++ resources/views/home/inc/links.blade.php
@@ -11,24 +11,27 @@
                             </div>
                         </div>
                     </a>
+                    <div style="background-color: #0083FF; height: 10px; width: 100%; border-radius: 5px; margin-top: 10px;"></div>
                 </div>
                 <div class="col-12 col-sm-4 mb-30 grid-item main-link-card">
                     <a href="{{ route('cita.con.el.amor') }}">
                         <div class="single-stories-card-content" style="padding-right: 10px; padding-left: 10px; padding-top: 39px; padding-bottom: 39px;">
                             <div class="text-center">
-                                <img src="{{ asset('images/cita-con-el-amor-final.png') }}" alt="Mundo Maxi" style="width: 100%; height: auto;">
+                                <img src="{{ asset('images/cita-con-el-amor-final.png') }}" alt="Cita con el amor" style="width: 100%; height: auto;">
                             </div>
                         </div>
                     </a>
+                    <div style="background-color: #F05D0F; height: 10px; width: 100%; border-radius: 5px; margin-top: 10px;"></div>
                 </div>
                 <div class="col-12 col-sm-4 mb-30 grid-item main-link-card">
                     <a href="{{ route('rnf') }}">
                         <div class="single-stories-card-content">
                             <div class="text-center">
-                                <img src="{{ asset('images/rock-n-foodball.png') }}" alt="Mundo Maxi" style="width: 100%; height: auto;">
+                                <img src="{{ asset('images/rock-n-foodball.png') }}" alt="RocknFoodball" style="width: 100%; height: auto;">
                             </div>
                         </div>
                     </a>
+                    <div style="background-color: #03CD40; height: 10px; width: 100%; border-radius: 5px; margin-top: 10px;"></div>
                 </div>
             </div>
         </div>
