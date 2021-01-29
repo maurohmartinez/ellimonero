c990de80fe6a24fd875e0c4d7d071a42937db5c5
Mauro Martinez <maurohmartinez@gmail.com>
2021-01-29 15:31:16 +0200

    More

--- resources/views/cita/inc/cards.blade.php
+++ resources/views/cita/inc/cards.blade.php
@@ -2,77 +2,150 @@
     <div class="container">
         <div class="spc-single-card-wrapper mb-4">
             <div class="spc-sc-img">
-                <a href="https://youtu.be/LZEFNUUJjMA" style="position: relative;">
+                <a target="_blank" href="https://youtu.be/LZEFNUUJjMA" style="position: relative;">
                     <i class="la la-youtube text-light" style="position: absolute; font-size: 50px; top: 50%; left: 50%; transform: translate(-50%, -50%);"></i>
-                    <img src="{{ asset('images/cita/leo_garcia.jpg') }}" alt="Leo García">
+                    <img src="{{ asset('images/cita/001.jpg') }}" alt="Leo García">
                 </a>
             </div>
             <div class="spc-sc-content-wrapper">
-                <!-- <div class="spc-scc-date">
-                    <h4>07 <span>nov</span></h4>
-                </div> -->
                 <div class="spc-scc-content">
                     <h5>Episodio 1</h5>
                     <h3>Leo García</h3>
-                    <p>Estamos acá para aprender qué es el amor.</p>
+                    <p>La unión.</p>
                     <a class="btn btn-primary mt-3" href="https://youtu.be/LZEFNUUJjMA"><i class="la la-youtube"></i> Ver episodio</a>
                 </div>
             </div>
         </div>
         <div class="spc-single-card-wrapper mb-4">
             <div class="spc-sc-img">
-                <a href="https://youtu.be/Cuo1Gq84HIU" style="position: relative;">
+                <a target="_blank" href="https://youtu.be/Cuo1Gq84HIU" style="position: relative;">
                     <i class="la la-youtube text-light" style="position: absolute; font-size: 50px; top: 50%; left: 50%; transform: translate(-50%, -50%);"></i>
-                    <img src="{{ asset('images/cita/miranda.jpg') }}" alt="Miranda">
+                    <img src="{{ asset('images/cita/002.jpg') }}" alt="Miranda">
                 </a>
             </div>
             <div class="spc-sc-content-wrapper">
-                <!-- <div class="spc-scc-date">
-                    <h4>23 <span>nov</span></h4>
-                </div> -->
                 <div class="spc-scc-content">
                     <h5>Episodio 2</h5>
                     <h3>Miranda</h3>
-                    <p>Cuando hacés las cosas con amor, el amor siempre vuelve.</p>
+                    <p>La Pasión y fiesta.</p>
                     <a class="btn btn-primary mt-3" href="https://youtu.be/Cuo1Gq84HIU"><i class="la la-youtube"></i> Ver episodio</a>
                 </div>
             </div>
         </div>
         <div class="spc-single-card-wrapper mb-4">
             <div class="spc-sc-img">
-                <a href="https://youtu.be/vW596BO2cxk" style="position: relative;">
+                <a target="_blank" href="https://youtu.be/ot8AvhvjR80" style="position: relative;">
                     <i class="la la-youtube text-light" style="position: absolute; font-size: 50px; top: 50%; left: 50%; transform: translate(-50%, -50%);"></i>
-                    <img src="{{ asset('images/cita/javier_calamaro.jpg') }}" alt="">
+                    <img src="{{ asset('images/cita/003.jpg') }}" alt="">
                 </a>
             </div>
             <div class="spc-sc-content-wrapper">
-                <!-- <div class="spc-scc-date">
-                    <h4>01 <span>ene</span></h4>
-                </div> -->
+                <div class="spc-scc-content">
+                    <h5>Episodio 3</h5>
+                    <h3>Sandra Mihanovich</h3>
+                    <p>Todo me recuerda a tí.</p>
+                    <a class="btn btn-primary mt-3" href="https://youtu.be/ot8AvhvjR80"><i class="la la-youtube"></i> Ver episodio</a>
+                </div>
+            </div>
+        </div>
+        <div class="spc-single-card-wrapper mb-4">
+            <div class="spc-sc-img">
+                <a target="_blank" href="https://youtu.be/vW596BO2cxk" style="position: relative;">
+                    <i class="la la-youtube text-light" style="position: absolute; font-size: 50px; top: 50%; left: 50%; transform: translate(-50%, -50%);"></i>
+                    <img src="{{ asset('images/cita/004.jpg') }}" alt="">
+                </a>
+            </div>
+            <div class="spc-sc-content-wrapper">
                 <div class="spc-scc-content">
                     <h5>Episodio 4</h5>
                     <h3>Javier Calamaro</h3>
-                    <p>El amor y la pasión son la misma cosa.</p>
+                    <p>La pasión.</p>
                     <a class="btn btn-primary mt-3" href="https://youtu.be/vW596BO2cxk"><i class="la la-youtube"></i> Ver episodio</a>
                 </div>
             </div>
         </div>
-        <div class="spc-single-card-wrapper">
+        <div class="spc-single-card-wrapper mb-4">
             <div class="spc-sc-img">
-                <a href="https://youtu.be/ZDr9M7PngOA" style="position: relative;">
+                <a target="_blank" href="https://youtu.be/HNAt0UK8nWY" style="position: relative;">
                     <i class="la la-youtube text-light" style="position: absolute; font-size: 50px; top: 50%; left: 50%; transform: translate(-50%, -50%);"></i>
-                    <img src="{{ asset('images/cita/fabi_cantilo.jpg') }}" alt="">
+                    <img src="{{ asset('images/cita/005.jpg') }}" alt="">
                 </a>
             </div>
             <div class="spc-sc-content-wrapper">
-                <!-- <div class="spc-scc-date">
-                    <h4>23 <span>nov</span></h4>
-                </div> -->
+
+                <div class="spc-scc-content">
+                    <h5>Episodio 5</h5>
+                    <h3>César Banana Pueyrredón</h3>
+                    <p>El amor no correspondido.</p>
+                    <a class="btn btn-primary mt-3" href="https://youtu.be/HNAt0UK8nWY"><i class="la la-youtube"></i> Ver episodio</a>
+                </div>
+            </div>
+        </div>
+        <div class="spc-single-card-wrapper mb-4">
+            <div class="spc-sc-img">
+                <a target="_blank" href="https://youtu.be/ZDr9M7PngOA" style="position: relative;">
+                    <i class="la la-youtube text-light" style="position: absolute; font-size: 50px; top: 50%; left: 50%; transform: translate(-50%, -50%);"></i>
+                    <img src="{{ asset('images/cita/006.jpg') }}" alt="">
+                </a>
+            </div>
+            <div class="spc-sc-content-wrapper">
+
                 <div class="spc-scc-content">
                     <h5>Episodio 6</h5>
-                    <h3>Fabi Cantilo</h3>
-                    <p>El amor nos va a salvar.</p>
+                    <h3>Fabiana Cantilo</h3>
+                    <p>El amor que nos rodea.</p>
                     <a class="btn btn-primary mt-3" href="https://youtu.be/ZDr9M7PngOA"><i class="la la-youtube"></i> Ver episodio</a>
+                </div>
+            </div>
+        </div>
+        <div class="spc-single-card-wrapper mb-4">
+            <div class="spc-sc-img">
+                <a target="_blank" href="https://youtu.be/2ETf1H8rVzI" style="position: relative;">
+                    <i class="la la-youtube text-light" style="position: absolute; font-size: 50px; top: 50%; left: 50%; transform: translate(-50%, -50%);"></i>
+                    <img src="{{ asset('images/cita/007.jpg') }}" alt="">
+                </a>
+            </div>
+            <div class="spc-sc-content-wrapper">
+
+                <div class="spc-scc-content">
+                    <h5>Episodio 7</h5>
+                    <h3>Manuel Wirtz</h3>
+                    <p>El enamoramiento.</p>
+                    <a class="btn btn-primary mt-3" href="https://youtu.be/2ETf1H8rVzI"><i class="la la-youtube"></i> Ver episodio</a>
+                </div>
+            </div>
+        </div>
+        <div class="spc-single-card-wrapper mb-4">
+            <div class="spc-sc-img">
+                <a target="_blank" href="https://youtu.be/ti_SZcUvx6g" style="position: relative;">
+                    <i class="la la-youtube text-light" style="position: absolute; font-size: 50px; top: 50%; left: 50%; transform: translate(-50%, -50%);"></i>
+                    <img src="{{ asset('images/cita/008.jpg') }}" alt="">
+                </a>
+            </div>
+            <div class="spc-sc-content-wrapper">
+
+                <div class="spc-scc-content">
+                    <h5>Episodio 7</h5>
+                    <h3>El Bahiano</h3>
+                    <p>El ciclo del amor.</p>
+                    <a class="btn btn-primary mt-3" href="https://youtu.be/ti_SZcUvx6g"><i class="la la-youtube"></i> Ver episodio</a>
+                </div>
+            </div>
+        </div>
+        <div class="spc-single-card-wrapper mb-4">
+            <div class="spc-sc-img">
+                <a target="_blank" href="https://youtu.be/gi0i5MvoMpE" style="position: relative;">
+                    <i class="la la-youtube text-light" style="position: absolute; font-size: 50px; top: 50%; left: 50%; transform: translate(-50%, -50%);"></i>
+                    <img src="{{ asset('images/cita/009.jpg') }}" alt="">
+                </a>
+            </div>
+            <div class="spc-sc-content-wrapper">
+
+                <div class="spc-scc-content">
+                    <h5>Episodio 7</h5>
+                    <h3>Ariel Puchetta</h3>
+                    <p>El desamor.</p>
+                    <a class="btn btn-primary mt-3" href="https://youtu.be/gi0i5MvoMpE"><i class="la la-youtube"></i> Ver episodio</a>
                 </div>
             </div>
         </div>
