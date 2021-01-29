cd0a53fd1992e4223ae7f21a362d8bf31458a980
Mauro Martinez <maurohmartinez@gmail.com>
2021-01-29 15:18:45 +0200

    More

--- resources/views/home/inc/modal.blade.php
+++ resources/views/home/inc/modal.blade.php
@@ -1,13 +1,13 @@
 <div class="modal fade" id="ganaEntradas" tabindex="-1" role="dialog" aria-labelledby="ganaEntradasTitle" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered" role="document">
-        <div class="modal-content" style="overflow: hidden !important;">
+        <div class="modal-content" style="overflow: hidden !important; border-radius: 10px; border-color: #1f1f1f; border-width: 2px;">
             <div>
                 <button style="position: absolute; right: 10px; top: 10px;" type="button" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true" class="badge badge-danger">&times;</span>
                 </button>
                 <img src="{{ asset('images/entradas.jpg') }}" alt="">
             </div>
-            <div class="p-4 bg-custom-table" style="border-radius: 0px;">
+            <div class="p-4" style="border-radius: 0px; background-color: #1f1f1f;">
                 @if($errors->hasAny())
                 <div class="alert alert-danger"><i class="la la-exclamation-circle mr-2"></i>
                     @if ($errors->has('general'))
