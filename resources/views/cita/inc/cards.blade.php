b33b74ecc83afadaa8ba2801e73ba9db1233fa2c
Mauro Martinez <maurohmartinez@gmail.com>
2021-01-29 19:06:34 +0200

    More.

--- resources/views/cita/inc/cards.blade.php
+++ resources/views/cita/inc/cards.blade.php
@@ -1,5 +1,6 @@
 <div class="spc-card-area pb-0 mb-0 pt-3">
     <div class="container">
+        <div class="p-4"></div>
         <div class="spc-single-card-wrapper mb-4">
             <div class="spc-sc-img">
                 <a target="_blank" href="https://youtu.be/LZEFNUUJjMA" style="position: relative;">
@@ -11,11 +12,12 @@
                 <div class="spc-scc-content">
                     <h5>Episodio 1</h5>
                     <h3>Leo García</h3>
-                    <p>La unión.</p>
+                    <p class="text-light">La unión.</p>
                     <a class="btn btn-primary mt-3" href="https://youtu.be/LZEFNUUJjMA"><i class="la la-youtube"></i> Ver episodio</a>
                 </div>
             </div>
         </div>
+        <div class="p-4"></div>
         <div class="spc-single-card-wrapper mb-4">
             <div class="spc-sc-img">
                 <a target="_blank" href="https://youtu.be/Cuo1Gq84HIU" style="position: relative;">
@@ -27,11 +29,12 @@
                 <div class="spc-scc-content">
                     <h5>Episodio 2</h5>
                     <h3>Miranda</h3>
-                    <p>La Pasión y fiesta.</p>
+                    <p class="text-light">La Pasión y fiesta.</p>
                     <a class="btn btn-primary mt-3" href="https://youtu.be/Cuo1Gq84HIU"><i class="la la-youtube"></i> Ver episodio</a>
                 </div>
             </div>
         </div>
+        <div class="p-4"></div>
         <div class="spc-single-card-wrapper mb-4">
             <div class="spc-sc-img">
                 <a target="_blank" href="https://youtu.be/ot8AvhvjR80" style="position: relative;">
@@ -43,11 +46,12 @@
                 <div class="spc-scc-content">
                     <h5>Episodio 3</h5>
                     <h3>Sandra Mihanovich</h3>
-                    <p>Todo me recuerda a tí.</p>
+                    <p class="text-light">Todo me recuerda a tí.</p>
                     <a class="btn btn-primary mt-3" href="https://youtu.be/ot8AvhvjR80"><i class="la la-youtube"></i> Ver episodio</a>
                 </div>
             </div>
         </div>
+        <div class="p-4"></div>
         <div class="spc-single-card-wrapper mb-4">
             <div class="spc-sc-img">
                 <a target="_blank" href="https://youtu.be/vW596BO2cxk" style="position: relative;">
@@ -59,11 +63,12 @@
                 <div class="spc-scc-content">
                     <h5>Episodio 4</h5>
                     <h3>Javier Calamaro</h3>
-                    <p>La pasión.</p>
+                    <p class="text-light">La pasión.</p>
                     <a class="btn btn-primary mt-3" href="https://youtu.be/vW596BO2cxk"><i class="la la-youtube"></i> Ver episodio</a>
                 </div>
             </div>
         </div>
+        <div class="p-4"></div>
         <div class="spc-single-card-wrapper mb-4">
             <div class="spc-sc-img">
                 <a target="_blank" href="https://youtu.be/HNAt0UK8nWY" style="position: relative;">
@@ -76,11 +81,12 @@
                 <div class="spc-scc-content">
                     <h5>Episodio 5</h5>
                     <h3>César Banana Pueyrredón</h3>
-                    <p>El amor no correspondido.</p>
+                    <p class="text-light">El amor no correspondido.</p>
                     <a class="btn btn-primary mt-3" href="https://youtu.be/HNAt0UK8nWY"><i class="la la-youtube"></i> Ver episodio</a>
                 </div>
             </div>
         </div>
+        <div class="p-4"></div>
         <div class="spc-single-card-wrapper mb-4">
             <div class="spc-sc-img">
                 <a target="_blank" href="https://youtu.be/ZDr9M7PngOA" style="position: relative;">
@@ -93,11 +99,12 @@
                 <div class="spc-scc-content">
                     <h5>Episodio 6</h5>
                     <h3>Fabiana Cantilo</h3>
-                    <p>El amor que nos rodea.</p>
+                    <p class="text-light">El amor que nos rodea.</p>
                     <a class="btn btn-primary mt-3" href="https://youtu.be/ZDr9M7PngOA"><i class="la la-youtube"></i> Ver episodio</a>
                 </div>
             </div>
         </div>
+        <div class="p-4"></div>
         <div class="spc-single-card-wrapper mb-4">
             <div class="spc-sc-img">
                 <a target="_blank" href="https://youtu.be/2ETf1H8rVzI" style="position: relative;">
@@ -110,11 +117,12 @@
                 <div class="spc-scc-content">
                     <h5>Episodio 7</h5>
                     <h3>Manuel Wirtz</h3>
-                    <p>El enamoramiento.</p>
+                    <p class="text-light">El enamoramiento.</p>
                     <a class="btn btn-primary mt-3" href="https://youtu.be/2ETf1H8rVzI"><i class="la la-youtube"></i> Ver episodio</a>
                 </div>
             </div>
         </div>
+        <div class="p-4"></div>
         <div class="spc-single-card-wrapper mb-4">
             <div class="spc-sc-img">
                 <a target="_blank" href="https://youtu.be/ti_SZcUvx6g" style="position: relative;">
@@ -125,13 +133,14 @@
             <div class="spc-sc-content-wrapper">
 
                 <div class="spc-scc-content">
-                    <h5>Episodio 7</h5>
+                    <h5>Episodio 8</h5>
                     <h3>El Bahiano</h3>
-                    <p>El ciclo del amor.</p>
+                    <p class="text-light">El ciclo del amor.</p>
                     <a class="btn btn-primary mt-3" href="https://youtu.be/ti_SZcUvx6g"><i class="la la-youtube"></i> Ver episodio</a>
                 </div>
             </div>
         </div>
+        <div class="p-4"></div>
         <div class="spc-single-card-wrapper mb-4">
             <div class="spc-sc-img">
                 <a target="_blank" href="https://youtu.be/gi0i5MvoMpE" style="position: relative;">
@@ -142,13 +151,14 @@
             <div class="spc-sc-content-wrapper">
 
                 <div class="spc-scc-content">
-                    <h5>Episodio 7</h5>
+                    <h5>Episodio 9</h5>
                     <h3>Ariel Puchetta</h3>
-                    <p>El desamor.</p>
+                    <p class="text-light">El desamor.</p>
                     <a class="btn btn-primary mt-3" href="https://youtu.be/gi0i5MvoMpE"><i class="la la-youtube"></i> Ver episodio</a>
                 </div>
             </div>
         </div>
+        <div class="p-3"></div>
         <a class="btn btn-primary btn-block my-4" href="/">Tienda Limonera</a>
     </div>
 </div>
